631d5f47222bb234300b894d296d62a5101c4f6e
mingqi830 <mqi@andrew.cmu.edu>
2015-08-17 17:02:17 -0700

    Edit search service execution log results page

--- public/javascripts/lastLoader.js
+++ public/javascripts/lastLoader.js
@@ -2,6 +2,7 @@ $(document).ready(function()
     { 
         $("#myTable").tablesorter(); 
         $("#myTable2").tablesorter(); 
+        $("#myTable3").tablesorter();
         $("#csTable").tablesorter(); 
     } 
 ); 
